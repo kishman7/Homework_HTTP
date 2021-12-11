f36e6c6393b8e2927729901078e35d9921438865
kishman7 <kishman_us@ukr.net>
2021-12-11 23:09:17 +0200

    Add Homework_HTTP

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Homework_HTTP
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var herous = UsingHttpWebRequest();
+            foreach (var item in herous)
+            {
+                lbHero.Items.Add(item);
+            }
+        }
+
+        private static List<Hero> UsingHttpWebRequest()
+        {
+            const string url = "http://hp-api.herokuapp.com/api/characters";
+            HttpWebRequest request = WebRequest.CreateHttp(url); //створюємо обєкт класа HttpWebRequest
+            var responce = (HttpWebResponse)request.GetResponse(); //отримуємо відповідь від request і розпарсуємо її до HttpWebRequest
+            var stream = responce.GetResponseStream();//повертає потік, який використовується для читання відповіді
+
+            var reader = new StreamReader(stream); //считуємо stream
+            var json = reader.ReadToEnd(); // і поміщаємо його в json
+
+            List<Hero> items = JsonConvert.DeserializeObject<List<Hero>>(json);//десереалізуємо у список Hero
+            return items;
+        }
+    }
+}
